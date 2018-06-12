922f90cc152a461baa1569c58277f0146f63302a
railken <railken@protonmail.ch>
2018-06-12 20:09:09 +0200

    basic seeder

---
+++ database/seeds/ListenerInvoiceIssuedSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Railken\LaraOre\Invoice\Database\Seeds;
+
+use Illuminate\Database\Seeder;
+
+class ListenerInvoiceIssuedSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        return 1;
+    }
+}
