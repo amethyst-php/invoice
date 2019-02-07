377d42fcfd859650b883a1f52c3a0fb4ec3b6584
railken <railken@protonmail.ch>
2019-02-07 21:50:28 +0100

    fix type_id InvoiceSchema

--- src/Schemas/InvoiceSchema.php
+++ src/Schemas/InvoiceSchema.php
@@ -30,7 +30,7 @@ class InvoiceSchema extends Schema
                 ->setRelationName('recipient')
                 ->setRelationManager(LegalEntityManager::class)
                 ->setRequired(true),
-            AmethystAttributes\TaxonomyAttribute::make('type', Config::get('amethyst.invoice.data.invoice.taxonomy'))
+            AmethystAttributes\TaxonomyAttribute::make('type_id', Config::get('amethyst.invoice.data.invoice.taxonomy'))
                 ->setRelationName('type'),
             AmethystAttributes\CountryAttribute::make('country')
                 ->setRequired(true),
