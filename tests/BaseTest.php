b9a4130fd566f8d633688c97bd8e8cf80e0f1a52
railken <railken@protonmail.ch>
2019-06-23 18:58:19 +0200

    fix test return type && fix composer.json

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -9,11 +9,8 @@ abstract class BaseTest extends \Orchestra\Testbench\TestCase
     /**
      * Setup the test environment.
      */
-    public function setUp()
+    public function setUp(): void
     {
-        $dotenv = new \Dotenv\Dotenv(__DIR__.'/..', '.env');
-        $dotenv->load();
-
         parent::setUp();
 
         File::cleanDirectory(database_path('migrations/'));
