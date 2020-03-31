2bd53bb64daef14f092d5e0d4ce0ede36c80cf8d
Railken <railken@protonmail.ch>
2020-04-01 00:23:56 +0200

    update data core

--- src/Providers/InvoiceServiceProvider.php
+++ src/Providers/InvoiceServiceProvider.php
@@ -16,8 +16,6 @@ class InvoiceServiceProvider extends CommonServiceProvider
     public function boot()
     {
         parent::boot();
-
-        app('amethyst')->pushMorphRelation('file', 'model', 'invoice');
     }
 
     /**
